ddbf466210845aafa3bd107829e80b54cb97c07a
mrfreddie04 <mrfreddie04@yahoo.com>
2021-12-15 14:53:42 -0500

    Update Stripe Session object structure

--- controllers/booking-controller.js
+++ controllers/booking-controller.js
@@ -34,31 +34,26 @@ module.exports.getCheckoutSession = catchAsync( async (req, res, next) => {
 
   //create checkout session (from stripe)
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    success_url: `${req.protocol==="https"?"https":"http"}://${req.get("host")}`,
-    //now handled by stripe webhook
-    //success_url: 
-    //`${req.protocol==="https"?"https":"http"}://${req.get("host")}/?user=${req.user._id}&tour=${tourid}&price=${tour.price }`,
-    cancel_url: `${req.protocol==="https"?"https":"http"}://${req.get("host")}/tour/${tour.slug}`,
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
-    client_reference_id: tourid,
+    client_reference_id: req.params.tourid,
+    mode: 'payment',
     line_items: [
       {
         quantity: 1,
         price_data: {
+          currency: 'usd',
           unit_amount: tour.price * 100,
-          currency: "usd",
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            //must be live images (hosted on the internet) 
-            images: [
-              `https://www.natours.dev/img/tours/${tour.imageCover}`
-            ]
-          }
-        }
-      }
-    ]
+            images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
+          },
+        },
+      },
+    ],
   });
 
   //send session to the client
